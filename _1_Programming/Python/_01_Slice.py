0261cc681f20989dde22e7905b6de420be371dbf
Vancheung <vancheung129@163.com>
2019-04-09 20:05:59 +0800

    Create _01_Slice.py

---
+++ _1_Programming/Python/_01_Slice.py
@@ -0,0 +1,22 @@
+def trim(s):
+    while(s[-1:] == ' '):
+        s = s[:-1]
+    while(s[:1] == ' '):
+        s = s[1:]
+    return s
+
+# 测试:
+if trim('hello  ') != 'hello':
+    print('测试失败!')
+elif trim('  hello') != 'hello':
+    print('测试失败!')
+elif trim('  hello  ') != 'hello':
+    print('测试失败!')
+elif trim('  hello  world  ') != 'hello  world':
+    print('测试失败!')
+elif trim('') != '':
+    print('测试失败!')
+elif trim('    ') != '':
+    print('测试失败!')
+else:
+    print('测试成功!')
