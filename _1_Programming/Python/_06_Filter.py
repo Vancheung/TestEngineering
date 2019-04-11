62eaa096f716d9928722ed85c89583f389c15a5e
Vancheung <vancheung129@163.com>
2019-04-11 14:38:58 +0800

    Create _06_Filter.py

---
+++ _1_Programming/Python/_06_Filter.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+def is_palindrome(n):
+    s = str(n)
+    for i in range(len(s)):
+        if i>len(s):
+            break
+        if s[i] != s[-i-1]:
+            return False
+    return True
+
+
+
+# 测试:
+output = filter(is_palindrome, range(1, 1000))
+print('1~1000:', list(output))
+if list(filter(is_palindrome, range(1, 200))) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 22, 33, 44, 55, 66, 77, 88, 99, 101, 111, 121, 131, 141, 151, 161, 171, 181, 191]:
+    print('测试成功!')
+else:
+    print('测试失败!')
