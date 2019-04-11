db47e13833cf196e8a322b116add21fd43a97871
Vancheung <vancheung129@163.com>
2019-04-11 16:13:29 +0800

    Create _08_Return_Function.py
    一个函数可以返回一个计算结果，也可以返回一个函数。
    返回一个函数时，牢记该函数并未执行，返回函数中不要引用任何可能会变化的变量。

---
+++ _1_Programming/Python/_08_Return_Function.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+def createCounter():
+    index = 0
+    def counter():
+        nonlocal index
+        index += 1
+        return index
+    return counter
+
+# 测试:
+counterA = createCounter()
+print(counterA(), counterA(), counterA(), counterA(), counterA()) # 1 2 3 4 5
+counterB = createCounter()
+if [counterB(), counterB(), counterB(), counterB()] == [1, 2, 3, 4]:
+    print('测试通过!')
+else:
+    print('测试失败!')
