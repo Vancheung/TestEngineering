e52311cfa96759e3168ab1f6023c03ff76bf4856
Vancheung <vancheung129@163.com>
2019-04-11 19:08:02 +0800

    Create _10_PrivateValue.py

---
+++ _1_Programming/Python/_10_PrivateValue.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+class Student(object):
+    def __init__(self, name, gender):
+        self.__name = name
+        self.__gender = gender
+    def get_gender(self):
+        return self.__gender
+    def set_gender(self,gender):
+        self.__gender = gender
+
+# 测试:
+bart = Student('Bart', 'male')
+if bart.get_gender() != 'male':
+    print('测试失败!')
+else:
+    bart.set_gender('female')
+    if bart.get_gender() != 'female':
+        print('测试失败!')
+    else:
+        print('测试成功!')
