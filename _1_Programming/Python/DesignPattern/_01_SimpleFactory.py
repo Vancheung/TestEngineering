730a2b9e9b5424748e4e5658c5968038a058a2c8
Vancheung <vancheung129@163.com>
2019-04-26 15:11:21 +0800

    Create _01_SimpleFactory.py

---
+++ _1_Programming/Python/DesignPattern/_01_SimpleFactory.py
@@ -0,0 +1,40 @@
+#coding:utf-8
+# 业务逻辑
+# 基类
+class Operation():
+    def __init__(self,numA,numB):
+        self.numA = numA
+        self.numB = numB
+# 加法类
+class Op_Add(Operation):
+    def __init__(self):
+        pass
+    def run(self):
+        return self.numA+self.numB
+
+# 减法类
+class Op_Minus(Operation):
+    def __init__(self):
+        pass
+    def run(self):
+        return self.numA-self.numB
+
+# 可扩展类
+
+# 工厂方法：在实例化工厂类时再取初始化运算类的对象
+def createOperate(op):
+    operators = {'+':Op_Add,'-':Op_Minus} #扩展时增加字典
+    return operators[op]
+
+# 界面逻辑
+class Calculate():
+    def __init__(self,numA, numB, op):
+        self.numA = numA
+        self.numB = numB
+        self.op = op
+    def getResult(self):
+        return createOperate(self.op).run(self)
+
+
+if __name__ == "__main__":
+    print(Calculate(3,5,'-').getResult())
