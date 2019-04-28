32bd86f3e1bf831d3e5d3dc6aa42e12344bc5a66
Vancheung <vancheung129@163.com>
2019-04-28 20:27:08 +0800

    Create _03_Decorator.py

---
+++ _1_Programming/Python/DesignPattern/_03_Decorator.py
@@ -0,0 +1,55 @@
+# 增加日志打印
+def log_wrapper(fun):
+    def wrapper(*args, **kwargs):
+        print('Before '+str(fun)+ ' Log')
+        ret = fun(*args, **kwargs)
+        print('After' +str(fun)+ ' Log')
+        return ret
+    return wrapper
+
+
+class Operation():
+    def __init__(self,numA,numB):
+        self.numA = numA
+        self.numB = numB
+# 加法类
+class Op_Add(Operation):
+    def __init__(self):
+        pass
+    @log_wrapper
+    def run(self):
+        return self.numA+self.numB
+
+# 减法类
+class Op_Minus(Operation):
+    def __init__(self):
+        pass
+    @log_wrapper
+    def run(self):
+        return self.numA-self.numB
+
+# 可扩展类
+
+# 工厂方法：在实例化工厂类时再取初始化运算类的对象
+@log_wrapper
+def createOperate(op):
+    #使用字典映射函数（python没有switch语句）
+    operators = {'+':Op_Add,'-':Op_Minus} #扩展时增加字典
+    return operators[op]
+
+
+# 界面逻辑
+class Calculate():
+    def __init__(self,numA, numB, op):
+        self.numA = numA
+        self.numB = numB
+        self.op = op
+
+    @log_wrapper
+    def getResult(self):
+        print('Caculating...')
+        return createOperate(self.op).run(self)
+
+
+if __name__ == "__main__":
+    print(Calculate(3,5,'-').getResult())
