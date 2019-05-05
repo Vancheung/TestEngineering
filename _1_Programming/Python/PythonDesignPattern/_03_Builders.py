83b5e94182d78b0d08aa220012720cd3eb4f0229
Vancheung <vancheung129@163.com>
2019-05-05 20:32:14 +0800

    Create _03_Builders.py

---
+++ _1_Programming/Python/PythonDesignPattern/_03_Builders.py
@@ -0,0 +1,124 @@
+from enum import Enum
+import time
+# 枚举可选项
+PizzaProgress = Enum('PizzaProgress', 'queued preparation baking ready')
+PizzaDough = Enum('PizzaDough', 'thin thick')
+PizzaSauce = Enum('PizzaSauce', 'tomato creme_fraiche')
+PizzaTopping = Enum('PizzaTopping', 'mozzarella double_mozzarella bacon ham mushrooms red_onion oregano')
+STEP_DELAY = 3
+
+# 产品类:最小化，只将数据初始化为合理的默认值
+class Pizza():
+    def __init__(self,name):
+        self.name = name
+        self.dough = None
+        self.sauce = None
+        self.topping = []
+
+    def __str__(self):
+        return self.name
+
+    def prepare_dough(self,dough):
+        self.dough = dough
+        print('preparing the {} dough of your {}...'.format(self.dough.name, self))
+        time.sleep(STEP_DELAY)
+        print('done with the {} dough'.format(self.dough.name))
+
+# 两个建造者类
+class MargaritaBuilder:
+    def __init__(self):
+        self.pizza = Pizza('margarita')
+        self.progress = PizzaProgress.queued
+        self.baking_time = 5  # 考虑是示例，单位为秒
+
+    def prepare_dough(self):
+        self.progress = PizzaProgress.preparation
+        self.pizza.prepare_dough(PizzaDough.thin)
+
+    def add_sauce(self):
+        print('adding the tomato sauce to your margarita...')
+        self.pizza.sauce = PizzaSauce.tomato
+        time.sleep(STEP_DELAY)
+        print('done with the tomato sauce')
+
+    def add_topping(self):
+        print('adding the topping (double mozzarella, oregano) to your margarita')
+        self.pizza.topping.append([i for i in (PizzaTopping.double_mozzarella,PizzaTopping.oregano)])
+        time.sleep(STEP_DELAY)
+        print('done with the topping (double mozzarella, oregano)')
+
+    def bake(self):
+        self.progress = PizzaProgress.baking
+        print('baking your margarita for {} seconds'.format(self.baking_time))
+        time.sleep(self.baking_time)
+        self.progress = PizzaProgress.ready
+        print('your margarita is ready')
+
+
+class CreamyBaconBuilder:
+    def __init__(self):
+        self.pizza = Pizza('creamy bacon')
+        self.progress = PizzaProgress.queued
+        self.baking_time = 7  # 考虑是示例，单位为秒
+
+    def prepare_dough(self):
+        self.progress = PizzaProgress.preparation
+        self.pizza.prepare_dough(PizzaDough.thick)
+
+    def add_sauce(self):
+        print('adding the crème fraîche sauce to your creamy bacon')
+        self.pizza.sauce = PizzaSauce.creme_fraiche
+        time.sleep(STEP_DELAY)
+        print('done with the crème fraîche sauce')
+
+    def add_topping(self):
+        print('adding the topping (mozzarella, bacon, ham, mushrooms, red onion, oregano) to your creamy bacon')
+        self.pizza.topping.append([t for t in (PizzaTopping.mozzarella,PizzaTopping.bacon,PizzaTopping.ham, PizzaTopping.mushrooms,
+                                           PizzaTopping.red_onion, PizzaTopping.oregano)])
+        time.sleep(STEP_DELAY)
+        print('done with the topping (mozzarella, bacon, ham, mushrooms, red onion, oregano)')
+
+    def bake(self):
+        self.progress = PizzaProgress.baking
+        print('baking your creamy bacon for {} seconds'.format(self.baking_time))
+        time.sleep(self.baking_time)
+        self.progress = PizzaProgress.ready
+        print('your creamy bacon is ready')
+
+# 指挥者类
+class Waiter:
+    def __init__(self):
+        self.builder = None
+
+    # 核心方法：接受一个建造者作为参数，以正确的顺序执行所有准备动作
+    # 无需修改Waiter代码即可实例化不同的Pizza类
+    def construct_pizza(self, builder):
+        self.builder = builder
+        [step() for step in (builder.prepare_dough, builder.add_sauce,
+                         builder.add_topping, builder.bake)]
+    @property
+    def pizza(self):
+        return self.builder.pizza
+
+# 控制有效输入
+def validate_style(builders):
+    try:
+        pizza_style = input('What pizza would you like, [m]argarita or [c]reamy bacon?')
+        builder = builders[pizza_style]()
+        valid_input = True
+    except KeyError as err:
+        print('Sorry, only margarita (key m) and creamy bacon (key c) are available')
+        return (False, None)
+    return (True, builder)
+
+def client():
+    builders = dict(m=MargaritaBuilder, c=CreamyBaconBuilder)
+    valid_input = False
+    while not valid_input:
+        valid_input, builder = validate_style(builders)
+    print()
+    waiter = Waiter()
+    waiter.construct_pizza(builder)
+
+if __name__ == '__main__':
+    client()
