841858f168e5566529855b056727c05e27c06b2b
Vancheung <vancheung129@163.com>
2019-05-27 19:15:34 +0800

    Update testFizzBuzz.py
    引入unittest库

--- _1_Programming/Python/TDD/Day3/testFizzBuzz.py
+++ _1_Programming/Python/TDD/Day3/testFizzBuzz.py
@@ -1,21 +1,23 @@
 import FizzBuzz as fz
+import unittest
 
 FIZZ = "Fizz"
 BUZZ = "Buzz"
 FIZZ_BUZZ = "FizzBuzz"
 
 
-def testFizzBuzz():
-    d = {}
-    d[1] = str(1)
-    d[3] = FIZZ
-    d[5] = BUZZ
-    d[13] = FIZZ
-    d[15] = FIZZ_BUZZ
 
-    for i in (1, 3, 5, 15, 13):
-        assert (d[i] == fz.FizzBuzz(i))
+class testFizzBuzz(unittest.TestCase):
+    def testFizzBuzz(self):
+        d = {}
+        d[1] = str(1)
+        d[3] = FIZZ
+        d[5] = BUZZ
+        d[13] = FIZZ
+        d[15] = FIZZ_BUZZ
+        for i in (1, 3, 5, 15, 13):
+            assert (d[i] == fz.FizzBuzz(i))
 
 
 if __name__ == '__main__':
-    testFizzBuzz()
+    unittest.main()
