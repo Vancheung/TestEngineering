83951e51dd469b1c69d354d6a30beb0eadaddd4e
Vancheung <vancheung129@163.com>
2019-06-05 20:36:19 +0800

    Update testArgs.py

--- _1_Programming/Python/TDD/Day6/testArgs.py
+++ _1_Programming/Python/TDD/Day6/testArgs.py
@@ -10,43 +10,26 @@ class testCases(unittest.TestCase):
     def testInitArgs(self):
         schema = Schema('%s' % sch)
         self.assertIsNotNone(schema)
-        # self.assertEqual(schema.parseSchema(), {'l': 'bool', 'p': 'int', 'd': 'str'})
 
     def testWrongSchema(self):
         try:
             schema = Schema('l:boo')
         except TypeError as e:
             print(e)
-    # def testIsMatchType(self):
-    #     arg = Args('%s' % sch)
-    #     df = defaultMap(arg.parseSchema())
-    #     print(df.dic)
-    #     print(df.getType('l'))
-    #     cmd = {'l':True}
-    #     [self.assertTrue(df.isBoolean(cmd[i], for i in cmd]
-    #     self.assertTrue(df.isBoolean(cmd))
 
-
-    def testIsBoolean(self):
-        """test defaultMap.isBoolean()
-        """
-        schema = Schema('%s' % sch)
-        # df = Schema('%s' % sch).df
+    def testIsBoolean(self):        
+        schema = Schema('%s' % sch)    
         self.assertTrue(schema.isBoolean('l',True))
         self.assertTrue(schema.isBoolean('l',False))
         self.assertFalse(schema.isBoolean('l',1))
 
-    def testIsInt(self):
-        """test defaultMap.isInt()
-        """
+    def testIsInt(self):        
         schema = Schema('%s' % sch)
         self.assertTrue(schema.isInt('p', 8000))
         self.assertFalse(schema.isInt('p', True))
         self.assertFalse(schema.isInt('p', 'str'))
 
     def testIsString(self):
-        """test defaultMap.isInt()
-        """
         schema = Schema('%s' % sch)
         self.assertFalse(schema.isString('d', 8000))
         self.assertFalse(schema.isString('d', True))
@@ -64,8 +47,6 @@ class testCases(unittest.TestCase):
             print(e)
 
     def testArgsSetProperty(self):
-        """
-        """
         schema = Schema(sch)
         arg = Args(schema,cmd)
         arg.cmdParse(cmd)
