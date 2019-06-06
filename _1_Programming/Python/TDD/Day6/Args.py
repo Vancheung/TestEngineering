265a936cde1b1b963c3ca4f45e58b29f0ef9cef6
Vancheung <vancheung129@163.com>
2019-06-06 10:56:03 +0800

    Update Args.py

--- _1_Programming/Python/TDD/Day6/Args.py
+++ _1_Programming/Python/TDD/Day6/Args.py
@@ -1,43 +1,42 @@
-class Args():
-    def __init__(self, schema, cmd):
-
-        self.dic = self.cmdParse(cmd)  # cmd解析为字典
-        self.schema = schema
-
+class Args:
+    def __init__(self, cmd):
+        self.cmd = self.cmdParse(cmd)
         self.logging = False
         self.port = None
         self.directory = None
 
-    # 检查cmd字典与schema是否匹配
-    def isMatch(self):
-        for i in self.schema.df:
-            if self.schema.df[i] == bool:
-                for j in self.dic[i]:
-                    if not self.schema.isBoolean(i, j):
-                        raise TypeError('Wrong argment type, [{}]\'s type should be bool.'.format(i))
-            if self.schema.df[i] == int:
-                for j in self.dic[i]:
-                    if not self.schema.isInt(i, j):
-                        raise TypeError('Wrong argment type, [{}]\'s type should be int.'.format(i))
-            if self.schema.df[i] == str:
-                for j in self.dic[i]:
-                    if not self.schema.isString(i, j):
-                        raise TypeError('Wrong argment type, [{}]\'s type should be string.'.format(i))
-        return True
-
     def cmdParse(self, cmd):
-        cmdDic = {}
+        d = {}
         for i in cmd.strip().split('-'):
             key = i.split(' ')[0]
+            values = i.strip().split(' ')[1:]
             if key:
-                cmdDic[i.split(' ')[0]] = i.strip().split(' ')[1:]
-        return cmdDic
+                d[key] = values
+        return d
 
-    def setProperty(self):
-        if 'l' in self.dic:
-            self.logging = True
-        if 'p' in self.dic:
-            self.port = int(self.dic['p'][0])
-        if 'd' in self.dic:
-            self.directory = self.dic['d'][0]
+    def isMatch(self, schema):
+        for i in self.cmd:
+            if schema.schema[i] == bool:
+                for j in self.cmd[i]:
+                    if not schema.isBool(j):
+                        raise TypeError('Wrong format,{} should be a boolean'.format(i))
 
+            if schema.schema[i] == int:
+                for j in self.cmd[i]:
+                    if not schema.isInt(j):
+                        raise TypeError('Wrong format,{} should be a int'.format(i))
+
+            if schema.schema[i] == str:
+                for j in self.cmd[i]:
+                    if not schema.isString(j):
+                        raise TypeError('Wrong format,{} should be a string'.format(i))
+        return True
+
+    def setProperty(self, schema):
+        if self.isMatch(schema):
+            if 'l' in self.cmd:
+                self.logging = True
+            self.port = int(self.cmd['p'][0])
+            self.directory = self.cmd['d'][0]
+        else:
+            raise TypeError('Wrong args for this schema!')
