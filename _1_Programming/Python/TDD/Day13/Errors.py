453ec26b8df895ebd823273376fd5e080a78593d
Vancheung <vancheung129@163.com>
2019-06-14 15:04:01 +0800

    Create Errors.py

---
+++ _1_Programming/Python/TDD/Day13/Errors.py
@@ -0,0 +1,13 @@
+class WrongIndexError(Exception):
+    pass
+
+class OutFieldError(Exception):
+    pass
+
+class InitError(Exception):
+    pass
+
+class WrongCommandError(Exception):
+    pass
+
+
