8074e34cebb576f3be8d2f178a9bc63980930746
Vancheung <vancheung129@163.com>
2019-06-14 15:03:04 +0800

    Update Field.py

--- _1_Programming/Python/TDD/Day13/Field.py
+++ _1_Programming/Python/TDD/Day13/Field.py
@@ -1,11 +1,23 @@
+from Location import Location
+from Errors import *
 class Field:
-    def __init__(self,X,Y):
-        self.X = X
-        self.Y = Y
+    def __init__(self,locationbegin,locationend):
+        self.locationbegin = locationbegin
+        self.locationend = locationend
         self.obstacles = []
 
-    def setObstacle(self,x,y):
-        self.obstacles.append((x,y))
+    def setObstacles(self,*kwargs):
+        for i in kwargs:
+            if self.isInField(Location(i[0],i[1])):
+                self.obstacles.append(i)
+            else:
+                raise OutFieldError
 
-    def getObstacle(self):
-        return self.obstacles
+    def isObstacle(self,location):
+        return (location.x,location.y) in self.obstacles
+
+    def __eq__(self, other):
+        return self.locationbegin==other.locationbegin and self.locationend == other.locationend
+
+    def isInField(self,location):
+        return self.locationbegin.x <= location.x <= self.locationend.x and self.locationbegin.y <= location.y <= self.locationend.y
