c456a60164c5e5d943c3793fa1a0815719e8fc90
Vancheung <vancheung129@163.com>
2019-09-30 17:12:04 +0800

    Create perf.py

---
+++ _1_Programming/Python/Samaritan/src/slave/perf.py
@@ -0,0 +1,31 @@
+from psutil import net_io_counters, disk_io_counters, disk_usage, \
+    virtual_memory, cpu_percent, process_iter
+
+
+def total_perf():
+    """
+    整机性能数据记录
+    :return:
+    """
+    return str((cpu_percent(),
+                virtual_memory().percent,
+                disk_usage('/').percent,
+                disk_io_counters().read_count,
+                disk_io_counters().write_count,
+                net_io_counters().packets_sent,
+                net_io_counters().packets_recv))
+
+
+def proc_perf(pname):
+    """
+    单进程性能记录
+    :param pname:
+    :return:
+    """
+    target = [p for p in process_iter(attrs=['pid', 'name']) if
+              pname in p.info['name']]
+    result = []
+    for p in target:
+        with p.oneshot():
+            result.append((p.name(), p.cpu_percent(), p.memory_info()))
+    return str(result)
