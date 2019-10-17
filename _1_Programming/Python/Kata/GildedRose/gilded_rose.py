5a9fd2e6cc2a61a82c35288911525387ff490ad7
Vancheung <vancheung129@163.com>
2019-10-17 19:33:08 +0800

    Create gilded_rose.py

---
+++ _1_Programming/Python/Kata/GildedRose/gilded_rose.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+class GildedRose(object):
+
+    def __init__(self, items):
+        self.items = items
+
+    def update_quality(self):
+        for item in self.items:
+            item.update_item()
+
+
