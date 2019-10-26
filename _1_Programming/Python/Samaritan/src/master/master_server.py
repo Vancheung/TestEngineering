3a5626b5a22f98d5f3e6bc7605dca627384dce16
Vancheung <vancheung129@163.com>
2019-10-26 18:03:36 +0800

    Create master_server.py

---
+++ _1_Programming/Python/Samaritan/src/master/master_server.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, g
+
+from src.DataOperation.MasterDb import MasterDb
+from src.master import DBPATH, get_ip_from_tbl_name
+
+DEBUG_SETTING = False
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    d = MasterDb(DBPATH)
+    g.db = d.conn
+    servers = d.get_table_names()
+    servers = set([get_ip_from_tbl_name(i) for i in servers])
+    return render_template('index.html', arr=servers)
+
+
+@app.route('/slave/<slave_ip>')
+def data(slave_ip):
+    d = MasterDb(DBPATH)
+    bar = d.draw(slave_ip)
+    return render_template('data.html', plot=bar)
+
+
+@app.teardown_request
+def teardown_request(exception):
+    g.db.close() if hasattr(g, 'db') else None
+
+
+if __name__ == '__main__':
+    print('Server is running......')
+    app.run(host='0.0.0.0', debug=DEBUG_SETTING)
