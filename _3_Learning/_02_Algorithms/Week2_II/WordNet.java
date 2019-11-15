4070843086be8dcca259779d87de2826bfc92312
Vancheung <vancheung129@163.com>
2019-11-15 17:57:19 +0800

    Update WordNet.java

--- _3_Learning/_02_Algorithms/Week2_II/WordNet.java
+++ _3_Learning/_02_Algorithms/Week2_II/WordNet.java
@@ -1,19 +1,17 @@
 import edu.princeton.cs.algs4.Bag;
 import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.DirectedCycle;
 import edu.princeton.cs.algs4.In;
 
 import java.util.HashMap;
 
 public class WordNet {
-
     private int vertexNum;
-
-    private final int root;
     private final SAP graphSap;
 
-    private Digraph digraph;
-    private HashMap<String, Bag<Integer>> nouns = new HashMap<>();
-    private HashMap<Integer, Bag<String>> idmap = new HashMap<>();
+    private final Digraph digraph;
+    private final HashMap<String, Bag<Integer>> nouns = new HashMap<>();
+    private final HashMap<Integer, String[]> idmap = new HashMap<>();
     // constructor takes the name of the two input files
 
     public WordNet(String synsets, String hypernyms) {
@@ -22,9 +20,12 @@ public class WordNet {
         readSynsets(synsets);
         digraph = new Digraph(vertexNum);
         readhyPernyms(hypernyms);
+        DirectedCycle directedCycle = new DirectedCycle(digraph);
+        if (directedCycle.hasCycle())
+            throw new IllegalArgumentException();
         graphSap = new SAP(digraph);
-        root = graphSap.findRoot();
     }
+
 
     private void readhyPernyms(String hypernyms) {
         In in = new In(hypernyms);
@@ -46,24 +47,14 @@ public class WordNet {
             int id = Integer.parseInt(token[0]);
             String[] linenouns = token[1].split(" ");
             this.vertexNum += 1;
-            Bag<String> words = new Bag<>();
             for (String word : linenouns) {
                 if (nouns.get(word) == null)
                     nouns.put(word, new Bag<>());
                 nouns.get(word).add(id);
-                words.add(word);
             }
-            idmap.put(id, words);
+            idmap.put(id, linenouns);
         }
 
-    }
-
-    public int getVertexNum() {
-        return vertexNum;
-    }
-
-    public int getRoot() {
-        return root;
     }
 
     // returns all WordNet nouns
@@ -80,7 +71,7 @@ public class WordNet {
     public int distance(String nounA, String nounB) {
         if (!isNoun(nounA) || !isNoun(nounB)) throw new IllegalArgumentException();
         Bag<Integer> idsA = nouns.get(nounA);
-        Bag<Integer> idsB  = nouns.get(nounB);
+        Bag<Integer> idsB = nouns.get(nounB);
 
         return graphSap.length(idsA, idsB);
     }
@@ -93,37 +84,12 @@ public class WordNet {
         Bag<Integer> ancestorB = nouns.get(nounB);
 
         int ancestor = graphSap.ancestor(ancestorA, ancestorB);
-        return concat(idmap.get(ancestor));
+        return String.join(" ", idmap.get(ancestor));
 
-    }
-
-    private String concat(Bag<String> strings) {
-        StringBuilder sb = new StringBuilder();
-        for (String s : strings) {
-            sb.append(s);
-        }
-        return sb.toString();
     }
 
     // do unit testing of this class
     public static void main(String[] args) {
-        String synsetsfile = "synsets15.txt";
-        String hypernymsfile = "hypernyms15Tree.txt";
-        WordNet wordNet = new WordNet(synsetsfile, hypernymsfile);
-        assert wordNet.vertexNum == 15 : "wrong vertexNum";
-        assert wordNet.root == 0 : "wrong root";
-        String result = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o";
-        int i = 0;
-        for (String noun : wordNet.nouns()) {
-            assert result.split(",")[i].equals(noun) : "wrong word";
-            i += 1;
-        }
-        assert wordNet.isNoun("a") : "a is Noun";
-        assert !wordNet.isNoun("z") : "z is not Noun";
-        // assert wordNet.sap("o", "l").equals("k") : "sap is not k";
-        assert wordNet.distance("h", "k") == 4 : "distance is not 4";
-        assert wordNet.sap("h", "k").equals("b") : "sap is not b";
-        assert wordNet.sap("o", "n").equals("n") : "sap is not n";
 
     }
 }
