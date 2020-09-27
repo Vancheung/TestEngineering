0cdc0dc973d5525a92eb4068d3ddbeb85f78c1c1
Vancheung <vancheung129@163.com>
2020-09-27 17:39:56 +0800

    Update __init__.py

--- _1_Programming/Python/Samaritan/src/__init__.py
+++ _1_Programming/Python/Samaritan/src/__init__.py
@@ -1,1 +1,10 @@
 __version__ = '0.0.1'
+from src.common.logger_utils import setup_logging
+from common.path_utils import PathUtil
+from sys import path as sys_path
+
+path_util = PathUtil()
+BASEDIR = path_util.root_path
+sys_path.append(BASEDIR)
+
+logger = setup_logging(BASEDIR + '/src/common/logging.json')
