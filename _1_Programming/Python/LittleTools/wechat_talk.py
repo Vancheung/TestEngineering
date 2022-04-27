62582566c04cf851c941c1d3b896207672b95e5b
Vancheung <vancheung129@163.com>
2022-04-27 14:55:12 +0800

    Create wechat_talk.py

---
+++ _1_Programming/Python/LittleTools/wechat_talk.py
@@ -0,0 +1,13 @@
+""" 安卓端微信摸鱼小工具 """ 
+
+ipmort uiautomator2
+
+client = uiautomator2.connect("")
+
+def send(context):
+    client(resourceId="com.tencent.mm:id/b4a").send_keys(context)
+    client(resourceId="com.tencent.mm:id/b8k").click()
+    
+
+def receive():
+    return [i.get_text() for i in client(className="android.widget.TextView")]
